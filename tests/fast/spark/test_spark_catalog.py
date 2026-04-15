80d291e299a71f3a46d07c6cb2fb2243ee92bbed
João Eiras <joao.eiras@gmail.com>
2026-04-15 11:24:12 +0200

    Further develop the Catalog API to match Spark better
    Implemented missing features.
    Added missing arguments.
    Corrected database vs schema handling.

--- tests/fast/spark/test_spark_catalog.py
+++ tests/fast/spark/test_spark_catalog.py
@@ -7,50 +7,130 @@ from spark_namespace.sql.catalog import Column, Database, Table
 
 
 class TestSparkCatalog:
-    def test_list_databases(self, spark):
+    def test_list_databases_all(self, spark):
         dbs = spark.catalog.listDatabases()
         if USE_ACTUAL_SPARK:
             assert all(isinstance(db, Database) for db in dbs)
         else:
             assert dbs == [
-                Database(name="memory", description=None, locationUri=""),
-                Database(name="system", description=None, locationUri=""),
-                Database(name="temp", description=None, locationUri=""),
+                Database(name="main", catalog="memory", description=None, locationUri=""),
+                Database(name="information_schema", catalog="system", description=None, locationUri=""),
+                Database(name="main", catalog="system", description=None, locationUri=""),
+                Database(name="pg_catalog", catalog="system", description=None, locationUri=""),
+                Database(name="main", catalog="temp", description=None, locationUri=""),
             ]
 
-    def test_list_tables(self, spark):
-        # empty
+    def test_create_use_schema(self, spark):
+        assert spark.catalog.currentDatabase() == "main"
+
+        spark.sql("CREATE SCHEMA my_schema1")
+        spark.catalog.setCurrentDatabase("my_schema1")
+        assert spark.catalog.currentDatabase() == "my_schema1"
+
+        dbs = spark.catalog.listDatabases("*schema1")
+        assert len(dbs) == 1
+        assert spark.catalog.currentDatabase() == "my_schema1"
+
+        if USE_ACTUAL_SPARK:
+            return
+
+        # Verifying the table goes to the right schema.
+        spark.sql("create table tbl1(a varchar)")
+        spark.sql("create table main.tbl2(a varchar)")
+        expected = [
+            Table(
+                name="tbl1",
+                catalog="memory",
+                database="my_schema1",
+                description=None,
+                tableType="TABLE",
+                isTemporary=False,
+            )
+        ]
+        tbls = spark.catalog.listTables()
+        assert tbls == expected
+
+        spark.sql("DROP TABLE my_schema1.tbl1")
+        spark.sql("DROP SCHEMA my_schema1")
+        assert len(spark.catalog.listDatabases("my_schema1")) == 0
+        assert spark.catalog.currentDatabase() == "main"
+
+    @pytest.mark.skipif(USE_ACTUAL_SPARK, reason="Checking duckdb specific databases")
+    def test_list_databases_pattern(self, spark):
+        expected = [
+            Database(name="pg_catalog", catalog="system", description=None, locationUri=""),
+        ]
+        dbs = spark.catalog.listDatabases("pg*")
+        assert dbs == expected
+        dbs = spark.catalog.listDatabases("pg_catalog")
+        assert dbs == expected
+        dbs = spark.catalog.listDatabases("notfound")
+        assert dbs == []
+
+    def test_list_tables_empty(self, spark):
         tbls = spark.catalog.listTables()
         assert tbls == []
 
-        if not USE_ACTUAL_SPARK:
-            # Skip this if we're using actual Spark because we can't create tables
-            # with our setup.
-            spark.sql("create table tbl(a varchar)")
-            tbls = spark.catalog.listTables()
-            assert tbls == [
-                Table(
-                    name="tbl",
-                    database="memory",
-                    description="CREATE TABLE tbl(a VARCHAR);",
-                    tableType="",
-                    isTemporary=False,
-                )
-            ]
+    @pytest.mark.skipif(USE_ACTUAL_SPARK, reason="Checking duckdb specific tables")
+    def test_list_tables_create(self, spark):
+        spark.sql("create table tbl1(a varchar)")
+        spark.sql("create table tbl2(b varchar); COMMENT ON TABLE tbl2 IS 'hello world'")
+        expected = [
+            Table(
+                name="tbl1", catalog="memory", database="main", description=None, tableType="TABLE", isTemporary=False
+            ),
+            Table(
+                name="tbl2",
+                catalog="memory",
+                database="main",
+                description="hello world",
+                tableType="TABLE",
+                isTemporary=False,
+            ),
+        ]
+        tbls = spark.catalog.listTables()
+        assert tbls == expected
+
+        tbls = spark.catalog.listTables(pattern="*l2")
+        assert tbls == expected[1:]
+
+        tbls = spark.catalog.listTables(pattern="tbl2")
+        assert tbls == expected[1:]
+
+        tbls = spark.catalog.listTables(dbName="notfound")
+        assert tbls == []
+
+        spark.sql("create view vw as select * from tbl1")
+        expected += [
+            Table(name="vw", catalog="memory", database="main", description=None, tableType="VIEW", isTemporary=False),
+        ]
+        tbls = spark.catalog.listTables()
+        assert tbls == expected
 
     @pytest.mark.skipif(USE_ACTUAL_SPARK, reason="We can't create tables with our Spark test setup")
     def test_list_columns(self, spark):
         spark.sql("create table tbl(a varchar, b bool)")
-        columns = spark.catalog.listColumns("tbl")
-        assert columns == [
-            Column(name="a", description=None, dataType="VARCHAR", nullable=True, isPartition=False, isBucket=False),
-            Column(name="b", description=None, dataType="BOOLEAN", nullable=True, isPartition=False, isBucket=False),
-        ]
 
-        # TODO: should this error instead?  # noqa: TD002, TD003
-        non_existant_columns = spark.catalog.listColumns("none_existant")
-        assert non_existant_columns == []
+        columns = spark.catalog.listColumns("tbl")
+        kwds = dict(description=None, nullable=True, isPartition=False, isBucket=False, isCluster=False)  # noqa: C408
+        assert columns == [Column(name="a", dataType="VARCHAR", **kwds), Column(name="b", dataType="BOOLEAN", **kwds)]
 
         spark.sql("create view vw as select * from tbl")
         view_columns = spark.catalog.listColumns("vw")
         assert view_columns == columns
+
+        from spark_namespace.errors import AnalysisException
+
+        with pytest.raises(AnalysisException):
+            assert spark.catalog.listColumns("tbl", "notfound")
+
+    def test_list_columns_not_found(self, spark):
+        from spark_namespace.errors import AnalysisException
+
+        with pytest.raises(AnalysisException):
+            spark.catalog.listColumns("none_existant")
+
+    def test_list_functions(self, spark):
+        fns = spark.catalog.listFunctions()
+        assert len(fns)
+        assert any(f.name == "current_database" for f in fns)
