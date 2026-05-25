32b37c1394898d677b4f0f47f8fad3b13d3cd4aa
João Rafael <joao.rafael@upperdelta.com>
2026-05-25 15:56:12 +0000

    Use non-view registry for Python register

--- src/duckdb_py/include/duckdb_python/python_replacement_scan.hpp
+++ src/duckdb_py/include/duckdb_python/python_replacement_scan.hpp
@@ -4,9 +4,24 @@
 #include "duckdb/common/case_insensitive_map.hpp"
 #include "duckdb/parser/tableref.hpp"
 #include "duckdb/function/replacement_scan.hpp"
+#include "duckdb_python/python_dependency.hpp"
 #include "duckdb_python/pybind11/pybind_wrapper.hpp"
 
 namespace duckdb {
+
+class PythonRegisteredObjectState : public ClientContextState {
+public:
+	static constexpr const char *Key = "python_registered_objects";
+
+	void Register(const string &name, const py::object &object);
+	void Unregister(const string &name);
+	py::object Get(const string &name);
+	bool Contains(const string &name);
+
+private:
+	mutex lock;
+	case_insensitive_map_t<shared_ptr<DependencyItem>> registered_objects;
+};
 
 struct PythonReplacementScan {
 public:
